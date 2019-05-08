975b2cc6ef474f6fac2ef9774421a05a28aa1401
NIkola Rendic <n.rendic993@gmail.com>
2019-05-08 17:55:33 +0200

    Refactoring done

--- src/main/java/com/infobip/web/urlshortener/controllers/AdminController.java
+++ src/main/java/com/infobip/web/urlshortener/controllers/AdminController.java
@@ -3,10 +3,11 @@ package com.infobip.web.urlshortener.controllers;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.infobip.web.urlshortener.domain.Website;
 import com.infobip.web.urlshortener.services.WebsiteService;
-import com.infobip.web.urlshortener.utilities.ActiveUserUtility;
+import com.infobip.web.urlshortener.utilities.CheckUrlUtility;
 import com.infobip.web.urlshortener.utilities.MappingValueUtility;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -23,22 +24,25 @@ public class AdminController {
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
-    public MappingJacksonValue registerUrl(@RequestBody Website website) throws Exception {
+    public MappingJacksonValue registerUrl(@RequestBody Website website, Authentication authentication) throws Exception {
         if (website.getUrl() == null || website.getUrl().equals("")) {
             throw new Exception("Url must not be null or empty string!");
         }
-        String activeUser = ActiveUserUtility.getActiveUser();
+        if (!CheckUrlUtility.checkUrl(website.getUrl())) {
+            throw new Exception("Url - " + website.getUrl() + " is not valid!");
+        }
+        String activeUser = authentication.getName();
         Website tmpWebsite = websiteService.registerUrl(website, activeUser);
         SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("shortUrl");
         return MappingValueUtility.mapValue("WebsiteFilter", filter, tmpWebsite);
     }
 
     @GetMapping("/statistic/{AccountId}")
-    public Map<String, Integer> findAllWebsitesById(@PathVariable String AccountId) throws Exception {
+    public Map<String, Integer> findAllWebsitesById(@PathVariable String AccountId, Authentication authentication) throws Exception {
         if (AccountId == null || AccountId.equals("")) {
             throw new Exception("Account id in request must not be null or empty!");
         }
-        String activeUser = ActiveUserUtility.getActiveUser();
+        String activeUser = authentication.getName();
         if (!AccountId.equals(activeUser)) {
             throw new Exception("Authenticated user is requesting statistic details of another user!");
         }
